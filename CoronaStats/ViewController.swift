565483841ea2ff91be7edd7ff378591163fb742f
Paula Leite <paula.t.leite@icloud.com>
2020-04-24 22:40:34 -0300

    Initial Commit

---
+++ CoronaStats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaStats
+//
+//  Created by Paula Leite on 24/04/20.
+//  Copyright © 2020 Paula Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
